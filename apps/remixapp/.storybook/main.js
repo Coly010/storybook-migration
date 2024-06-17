d8a4e4792eaaa26c20a70e89c02c994158169f5b
Colum Ferry <cferry09@gmail.com>
2024-06-17 15:38:01 +0100

    add storybook configurations

---
+++ apps/remixapp/.storybook/main.js
@@ -0,0 +1,18 @@
+const config = {
+  stories: ['../src/app/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {
+      builder: {
+        viteConfigPath: 'apps/remixapp/vite.config.ts',
+      },
+    },
+  },
+};
+
+export default config;
+
+// To customize your Vite configuration you can use the viteFinal field.
+// Check https://storybook.js.org/docs/react/builders/vite#configuration
+// and https://nx.dev/recipes/storybook/custom-builder-configs
